fa15d094de13306f9fc33fdcb00964f7b1dc6ff8
sabithulbananinoor <sabithul.banani.noor@gmail.com>
2024-06-25 03:05:57 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Dosen.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen {
+    public static void tampilanInfo() {
+        System.out.println();
+        System.out.println("<============ Data Dosen ============>");
+        System.out.println("NIDN : 110305802");
+        System.out.println("Nama : Mirza Yogy Kuniawan");
+        System.out.println("<====================================>");
+    }
+}
---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa {
+    public static void tampilanInfo() {
+        System.out.println();
+        System.out.println("<============ Data Mahaiswa ============>");
+        System.out.println("NPM : 2010010426");
+        System.out.println("Nama : Sabithul Banani Noor");
+        System.out.println("<=======================================>");
+    }
+}
